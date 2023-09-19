547cad76852728a016d8d1bf66664bfe2295fad7
Dynamo285K <113776601+Dynamo285K@users.noreply.github.com>
2023-09-19 19:32:35 +0200

    Add files via upload
    pridana uloha 7

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import random
-# students = input('zadaj pocet studentov')
-# questions = input('zadaj pocet otazok')
-#
-# students = int(students)
-# questions = int(questions)
+from random import choice
+import sys
 
-students = 20
-questions = 20
+fr = open('bus_vytazenost.txt', 'w', encoding='utf-8' )
 
-print(students,questions)
+students = int(input('zadaj pocet studentov'))
+questions = int(input('zadaj pocet otazok'))
 
-fr = open("result.txt", "w", encoding="utf-8")
-r = 0
-temp = []
-randomList1 = []
-randomList2 = []
-with open('result.txt') as fp:
-    for i in range (1,students+1):
-        i == str(i)
-        x = str(random.randrange(1,questions+1))
-        while x in randomList1:
-             x = str(random.randrange(1,questions+1))
-        y = str(random.randrange(1,students+1))
-        while y in randomList2:
-            y = str(random.randrange(1,students+1))
-        res = str(i)+'. student: '+y+', otazka:'+x
-        res2 = '\n'
-        # fr.write(str(i)+'. student: '+y+', otazka:'+x+'\n')
-        temp = res.split(':')
-        if int(temp[-1]) % 2 != r % 2:
-            fr.write(res+res2)
-            randomList1.append(x)
-            randomList2.append(y)
-            r == int(x)
+if students > questions:
+    sys.exit('pozor studentov je viac ako otazok')
 
+list_of_students = list(range(1,students+1))
+random.shuffle(list_of_students)
+# print(list_of_students)
 
+list_of_questions = list(range(1,questions+1))
+random.shuffle(list_of_questions)
+# print(list_of_questions)
 
+questions_result = [list_of_questions[0]]
+for i in list_of_questions[1::]:
+    indexos = list_of_questions.index(i)
+    delenie = i % 2
+    if questions_result[indexos-1] % 2 != delenie:
+        questions_result.append(i)
+    else:
+        my_random_int = choice(list(set(range(1, questions+1)) - set(questions_result)))
+        while my_random_int % 2 == questions_result[indexos-1] % 2:
+            my_random_int = choice(list(set(range(1, questions+1)) - set(questions_result)))
+        else:
+            questions_result.append(my_random_int)
+print('questions_result',questions_result)
+
+with open('result.txt', 'w') as fp:
+    for i in range(students):
+        if i == students:
+            fp.write(str(i+1)+'. student: '+str(list_of_students[i])+', otazka:'+str(questions_result[i]))
+        else:
+            fp.write(str(i+1)+'. student: '+str(list_of_students[i])+', otazka:'+str(questions_result[i])+'\n')
+        # if i == students:
+        #     fp.write(str(i+1)+'. student: '+str(list_of_students[i])+', otazka:'+str(list_of_questions[i]))
+        # else:
+        #     fp.write(str(i+1)+'. student: '+str(list_of_students[i])+', otazka:'+str(list_of_questions[i])+'\n')
